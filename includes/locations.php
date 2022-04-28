f24c543d3426d45c1a7c24c021697f67d4034543
andrebernardino <andrebernardino@gmail.com>
2022-04-28 10:30:57 -0300

    first commit

---
+++ includes/locations.php
@@ -0,0 +1,9 @@
+<h2>Locations:</h2>
+<div class="row">
+  <div class="col-lg-2"><center><img src="https://via.placeholder.com/150x150" width="150" class="img-circle" alt="New York"><br /><a href="">New York</a></center></div>
+  <div class="col-lg-2"><center><img src="https://via.placeholder.com/150x150" width="150" class="img-circle" alt="San Francisco"><br /><a href="">San Francisco</a></center></div>
+  <div class="col-lg-2"><center><img src="https://via.placeholder.com/150x150" width="150" class="img-circle" alt="Paris"><br /><a href="">Paris</a></center></div>
+  <div class="col-lg-2"><center><img src="https://via.placeholder.com/150x150" width="150" class="img-circle" alt="New York"><br /><a href="">New York</a></center></div>
+  <div class="col-lg-2"><center><img src="https://via.placeholder.com/150x150" width="150" class="img-circle" alt="San Francisco"><br /><a href="">San Francisco</a></center></div>
+  <div class="col-lg-2"><center><img src="https://via.placeholder.com/150x150" width="150" class="img-circle" alt="Paris"><br /><a href="">Paris</a></center></div>
+</div> 
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
+
+    <title>Page Title</title>
+  </head>
+  <body>
+<div class="container">
+<!--CONTENT GOES HERE-->
+<?php include("includes/menusuperior.php");?>
+    <h1>Hello, world!</h1>
+<h2>Welcome to my website</h2>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+       <h3>Column 1</h3> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+
+      <?php include("includes/locations.php");?>
+      
+      </div>
+    </div>
+  </div>
+
+<!--CONTENT ENDS HERE-->
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
